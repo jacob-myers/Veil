d105ccad5c509e41c93d06965ce7c4ae6be082f6
jacob-myers <jakemyers190@gmail.com>
2024-01-06 15:19:05 -0500

    Pulling up isValidShiftPair to cipher_shift.dart 	Necessary for new shift page.

--- lib/functions/cipher_shift.dart
+++ lib/functions/cipher_shift.dart
@@ -7,8 +7,30 @@ Cryptext cryptShift(Cryptext cryptext, int shift) {
   return Cryptext.fromIntList(intVecFinal, alphabet : cryptext.alphabet);
 }
 
+/// Given two pieces of Cryptext, determines if there is a shift that can turn
+/// p into c by applying the shift cipher.
+bool isValidShiftPair(Cryptext p, Cryptext c) {
+  if (p.length == 0 || c.length == 0) {
+    return false;
+  }
+  if (p.length != c.length) {
+    return false;
+  }
+  if (!p.isExclusiveToAlphabet || !c.isExclusiveToAlphabet) {
+    return false;
+  }
+
+  int shift = p.alphabet.mod(c.numeralized[0] - p.numeralized[0]);
+  for (int i = 0; i < p.length; i++) {
+    if (p.alphabet.mod(c.numeralized[i] - p.numeralized[i]) != shift) {
+      return false;
+    }
+  }
+  return true;
+}
+
 /// Testing function to see if the shift cipher worked.
-void testCryptShift() {
+void main() {
   Cryptext plaintext = Cryptext.fromString('ABCDEFGHIJKLMNOPQRSTUVWXYZ');
   print(cryptShift(plaintext, 1));
 }
